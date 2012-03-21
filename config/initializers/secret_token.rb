c6d5ad8be9dd4ec65002a8ee2012eec8889118a0
n-jax <n@j.com>
2012-03-21 15:02:18 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Perftest::Application.config.secret_token = 'ac8ae8688e6907dee78e2fd122c9429ac74345d0de1db5e91e2b903f987f0912c9ab454e10f841ba37a4166e1aa6ed20f86a5911a4bb4843a3d01a1658823318'
